91a329e537d4dd64e68801a73e7fbb4066df8c62
yuan enguang <yuaneg@users.noreply.github.com>
2023-04-21 10:29:06 +0800

    Update cf_worker.js

--- cf_worker.js
+++ cf_worker.js
@@ -1,4 +1,4 @@
-const TELEGRAPH_URL = 'https://api.openai.com';
+const TELEGRAPH_URL = 'https://chat.openai.com';
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
